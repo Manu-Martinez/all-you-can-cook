2f0efe7538a00d044379223c664d909270af5425
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-10-11 21:25:04 -0300

    added Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>All you can cook</h1>
+      </div>
+      <a>Home</a>
+      <a>Recipe</a>
+      <a>About</a>
+    </nav>
+  );
+};
+
+export default Navbar;
