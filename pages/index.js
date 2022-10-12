093d8722d305ab855f5d541b16a338f77a4a7b86
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-10-11 22:44:20 -0300

    updated index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import Link from 'next/link'
+import Recipe from "./Details/recipe";
 
 export default function Home() {
   return (
@@ -7,6 +9,7 @@ export default function Home() {
       <Navbar></Navbar>
 	  <h1>Homepage</h1>
 	  <p>SDASDASDASFSDFS</p>
+	  <Link href="/recipe"><a>See full recipe</a></Link>
 	  <Footer></Footer>
     </div>
   );
